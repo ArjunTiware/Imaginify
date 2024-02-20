bbbf9b8fa0b911baa6884db81f7870918ee57a39
Arjun Tiwari <126322848+ArjunTiware@users.noreply.github.com>
2024-02-20 15:29:07 +0530

    initial commit

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTransformationPageType = () => {
+  return (
+    <div>
+      AddTransformationPageType
+    </div>
+  )
+}
+
+export default AddTransformationPageType
